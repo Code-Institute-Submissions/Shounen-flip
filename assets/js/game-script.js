5a8d81f8a4e8954f2b7d57c767ac4a17f89d7691
EC2 Default User <ec2-user@ip-172-31-16-96.ec2.internal>
2019-09-29 14:52:34 +0000

    edited game script and added more backgrounds and fixed close buttons

--- assets/js/game-script.js
+++ assets/js/game-script.js
@@ -1,99 +1,128 @@
-
-
-
 //variables 
 
 
 
 const cards = document.querySelectorAll(".memory-card");
 
-const ranks = [ "A", "B", "C","D"  ]; // to comit 
+const ranks = ["A", "B", "C", "D"]; // to comit 
 
 let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard;
 
-var turn = 0 ;
+var turn = 0;
 var counter = document.getElementById("counter")
-var timerInterval = 0; 
-var numOfMatchedCards = 0 ;
+var timerInterval = 0;
+var numOfMatchedCards = 0;
 var rank_score_display = document.getElementById("rank");
 
 
 window.onload = shuffle();
 
 
+
 //main game/reset button //
- document.getElementById("reset-btn").addEventListener( "click",   function(){
-       resetGame()   
-       hideModel() 
-    });
+document.getElementById("reset-btn").addEventListener("click", function() {
+    resetGame();
+    hideModel();
+});
+
+// how to play button 
+document.getElementById("how-to-play-modal-reset-btn").addEventListener("click", function() {
+    displayHowToPlayModal();
+});
+
 
 
 /// lose modal reset button //
- document.getElementById("lose-modal-reset-btn").addEventListener( "click",   function(){
-       resetGame()   
-       hideModel() 
+document.getElementById("lose-modal-reset-btn").addEventListener("click", function() {
+    resetGame();
+    hideLoseModal();
+});
+
+///win  modal reset button //
+document.getElementById("win-modal-reset-btn").addEventListener("click", function() {
+    resetGame();
+   hideWinModel();
+});
+
+
+//had some problems with the close button on the how to play modal so I had to create a seperate  event listener.  .
+
+//click, trigger function and close 
+
+
+document.getElementById("close-btn1").addEventListener("click",  function() {
+       hideWinModel();
+    });
+document.getElementById("close-btn2").addEventListener("click",  function() {
+       hideLoseModal();
+    });
+document.getElementById("close-btn3").addEventListener("click",  function() {
+        hideHowToPlayModal();
     });
 
-    ///win  modal reset button //
- document.getElementById("win-modal-reset-btn").addEventListener( "click",   function(){
-    resetGame()   
-    hideModel() 
- });
- 
- 
- //// modal close/display functions 
- 
- function displayLoseModal() { 
-   document.querySelector(".bg-lose-modal").style.display = "flex";
+
+//// modal close/display functions 
+
+function displayHowToPlayModal() {
+    document.querySelector(".bg-how-to-play-modal").style.display = "flex";
+}
+
+function displayLoseModal() {
+    document.querySelector(".bg-lose-modal").style.display = "flex";
 }
 
 
-function hideModel() { 
-    document.querySelector(".bg-lose-modal").style.display = "none";
+function hideWinModel() {
+   
     document.querySelector(".bg-win-modal").style.display = "none";
 
 }
 
-document.querySelector(".close").addEventListener("click",
-function(){
-    hideModel()   
+function hideLoseModal() {
+      document.querySelector(".bg-lose-modal").style.display = "none";
 }
-);
- 
- 
-  /// reset game function
- 
- 
- function resetGame() {
-//add event listener 
-cards.forEach(card => card.addEventListener("click", flipcard)); 
-// reset matched cards
-numOfMatchedCards = 0 ;
+// 
+function hideHowToPlayModal() {
+console.log("test close");
+    document.querySelector(".bg-how-to-play-modal").style.display = "none";
+}
 
-//remove class 
-$(".memory-card").removeClass('flip');//had to use JQuery here in order to remove this class and reset the game. 
 
-//shuffle board
 
-shuffle();
+
+/// reset game function
+
+
+function resetGame() {
+    //add event listener 
+    cards.forEach(card => card.addEventListener("click", flipcard));
+    // reset matched cards
+    numOfMatchedCards = 0;
+
+    //remove class 
+    $(".memory-card").removeClass('flip'); //had to use JQuery here in order to remove this class and reset the game. 
+
+    //shuffle board
+
+    shuffle();
 
     //reset moves
-    turn = 0 ;
+    turn = 0;
     counter.innerHTML = "Turn:" + turn;
 
     //reset timer
 
- timer.innerHTML = "Time:" + "02" + ";" + "30" 
- clearInterval(timerInterval)
- timerInterval = 0;
- timeleft = 180;
- timeCounter = 0;
+    timer.innerHTML = "Time:" + "02" + ";" + "30"
+    clearInterval(timerInterval)
+    timerInterval = 0;
+    timeleft = 180;
+    timeCounter = 0;
 
-// reset rank 
-var rank_score_display = document.getElementById("rank");
-rank_score_display.innerHTML = "Rank:" + ranks[0];
+    // reset rank 
+    var rank_score_display = document.getElementById("rank");
+    rank_score_display.innerHTML = "Rank:" + ranks[0];
 
 }
 
@@ -130,14 +159,14 @@ function checkForMatch() {
     let isMatch = firstCard.dataset.framework ===
         secondCard.dataset.framework;
 
-        if(firstCard.dataset.framework ===
-            secondCard.dataset.framework){
- matchedCards() 
+    if (firstCard.dataset.framework ===
+        secondCard.dataset.framework) {
+        matchedCards()
 
-        }
+    }
 
     isMatch ? disableCards() : unflipCards(); // <- This is a ternery operator which allow you to write an if else statement in one line.
-  
+
 }
 
 function disableCards() {
@@ -183,19 +212,19 @@ cards.forEach(card => card.addEventListener("click", flipcard));
 
 // changes to commit: timer code and turnCounter code.
 
-function turnCounter(){
-
-    
-turn++;
-counter.innerHTML = "Turns:" + turn;
-
-if (turn == 1 ){
-    
-    beginTimer();
-    
+function turnCounter() {
 
 
-}
+    turn++;
+    counter.innerHTML = "Turns:" + turn;
+
+    if (turn == 1) {
+
+        beginTimer();
+
+
+
+    }
 
 
 }
@@ -203,13 +232,13 @@ if (turn == 1 ){
 
 
 ////// Number of matched cards /////
-    
+
 
 
 function matchedCards() {
     numOfMatchedCards++;
 
-    if (numOfMatchedCards == 6 ){
+    if (numOfMatchedCards == 6) {
         clearInterval(timerInterval)
         win()
     }
@@ -219,106 +248,101 @@ function matchedCards() {
 /////////////timer code //////////////////////
 
 var timeleft = 150;
- 
-    var timer = document.getElementById("timer")
 
-var timeCounter = 0 ; 
+var timer = document.getElementById("timer")
+
+var timeCounter = 0;
 //https://www.youtube.com/watch?v=MLtAMg9_Svw
 
-function convertTime (s) {
+function convertTime(s) {
     var mins = parseInt(s / 60);
     var secs = parseInt(s % 60);
-    
 
-    if (mins + secs  == 0){
+
+    if (mins + secs == 0) {
         displayLoseModal()
-   clearInterval(timerInterval)
-     timeleft = 150;
-       timeCounter = 0;
+        clearInterval(timerInterval)
+        timeleft = 150;
+        timeCounter = 0;
         timerInterval = 0;
-        
-     }
 
-     var rank_score_display = document.getElementById("rank");
-
-     if(mins == 02 && secs  == 30 ) { 
-     rank_score_display.innerHTML = "Rank:" + ranks[0];
-     
-     }
-    
-     if(mins ==01 && secs  == 45 ) {
-     rank_score_display.innerHTML = "Rank:" + ranks[1];
-     
-     }
-    
-     if(mins ==01 && secs  == 00 ) {
-     rank_score_display.innerHTML = "Rank:" + ranks[2];
-     
-     }
-    
-     if(mins ==00 && secs  == 20  ) { 
-        rank_score_display.innerHTML = "Rank:" + ranks[3];
-     
-     
-     }
-
-
-          
-    if( secs < 10) { 
-        return  "0" + mins + ":" + "0" + secs ;
-        }
-       
-      
-
-    else { 
-        return  "0" + mins + ":" + secs ;
-    
     }
-    
-       return  mins + ":" + secs ;
-    }; 
+
+    var rank_score_display = document.getElementById("rank");
+
+    if (mins == 02 && secs == 30) {
+        rank_score_display.innerHTML = "Rank:" + ranks[0];
+
+    }
+
+    if (mins == 01 && secs == 45) {
+        rank_score_display.innerHTML = "Rank:" + ranks[1];
+
+    }
+
+    if (mins == 01 && secs == 00) {
+        rank_score_display.innerHTML = "Rank:" + ranks[2];
+
+    }
+
+    if (mins == 00 && secs == 20) {
+        rank_score_display.innerHTML = "Rank:" + ranks[3];
+
+
+    }
+
+
+
+    if (secs < 10) {
+        return "0" + mins + ":" + "0" + secs;
+    }
+
+
+
+    else {
+        return "0" + mins + ":" + secs;
+
+    }
+
+    return mins + ":" + secs;
+};
 
 
 function beginTimer() {
 
-    timerInterval = 0; 
+    timerInterval = 0;
     var timeleft = 150;
 
-if (timerInterval == 0 ) {
-//changes display for timer . 
-    timer.innerHTML = "Time" +  ":" + convertTime(timeleft - timeCounter);
-    
-
-timerInterval = setInterval( function startTimer() {
-        timeCounter++;
+    if (timerInterval == 0) {
+        //changes display for timer . 
         timer.innerHTML = "Time" + ":" + convertTime(timeleft - timeCounter);
-       } , 1000);
-    
+
+
+        timerInterval = setInterval(function startTimer() {
+            timeCounter++;
+            timer.innerHTML = "Time" + ":" + convertTime(timeleft - timeCounter);
+        }, 1000);
+
     }
- 
+
 }
 
 
 //// win pop up///
 
-function win(){
+function win() {
     document.querySelector(".bg-win-modal").style.display = "flex";
 
-var finalRank = rank_score_display.innerHTML;
- var   finalTime = timer.innerHTML;
-  var   finalMoves = counter.innerHTML;
+    var finalRank = rank_score_display.innerHTML;
+    var finalTime = timer.innerHTML;
+    var finalMoves = counter.innerHTML;
 
 
 
     document.getElementById("finalRankScore").innerHTML = finalRank;
     document.getElementById("finalTimeScore").innerHTML = "Time:" + finalTime;
-    document.getElementById("finalMovesScore").innerHTML = finalMoves ;
+    document.getElementById("finalMovesScore").innerHTML = finalMoves;
 
 
 
 }
-
-
- 
-
-
